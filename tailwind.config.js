41ed33ea24f52c1395a9a5043dbed66cacc9bd63
jaesukpark77 <jaesukpark77@naver.com>
2022-12-07 17:55:51 +0900

    Feat: (mobile) header__info 마크업

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,17 @@ module.exports = {
   content: ['./src/**/*.html'],
   theme: {
     fontSize:{
-      'logBtn' : ['12px', '18px']
+      'logBtn' : ['12px', '18px'],
+      'headerInfo1' : ['34px', '40.8px'],
+      'headerInfo2' : ['18px', '27px'],
+      'headerInfo3' : ['14px', '21px'],
+      'headerInfo4' : ['12px', '18px'],
     },
     fontFamily:{
       'spoqahansans': ['Spoqa Han Sans Neo', 'sans-serif'],
+    },
+    borderRadius:{
+      
     },
     extend: {},
   },
