1bde712af2688d1808627cc15439d92e352d2d11
jaesukpark77 <jaesukpark77@naver.com>
2022-12-07 16:54:05 +0900

    Feat: (mobile) header의 header__top 구현
    tailwind를 활용

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ['./src/**/*.html'],
   theme: {
+    fontSize:{
+      'logBtn' : ['12px', '18px']
+    },
     fontFamily:{
       'spoqahansans': ['Spoqa Han Sans Neo', 'sans-serif'],
     },
